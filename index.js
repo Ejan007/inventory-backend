c96309de7fa115984159716a5295050f0685626f
Ejan007 <ejancool12@gmail.com>
2025-03-12 22:32:00 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors =  require('cors');
+const { PrismaClient } = require('@prisma/client');
+const figlet = require('figlet');
+
+const app = express();
+const prisma = new PrismaClient();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+// Create an inventory item
+app.post('/items', async (req, res) => {
+  const { name, quantity } = req.body;
+  try {
+    const item = await prisma.item.create({
+      data: { name, quantity },
+    });
+    res.json(item);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to add item' });
+  }
+});
+
+// Get all inventory items
+app.get('/items', async (req, res) => {
+  try {
+    const items = await prisma.item.findMany();
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve items' });
+  }
+});
+
+// Update an inventory item
+app.put('/items/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, quantity } = req.body;
+  try {
+    const item = await prisma.item.update({
+      where: { id: Number(id) },
+      data: { name, quantity },
+    });
+    res.json(item);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update item' });
+  }
+});
+
+// Delete an inventory item
+app.delete('/items/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const item = await prisma.item.delete({
+      where: { id: Number(id) },
+    });
+    res.json(item);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete item' });
+  }
+});
+
+app.listen(PORT, () => {
+    figlet('StockIT', (err, data) => {
+      if (err) {
+        console.log('Something went wrong with figlet...');
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+      console.log(`Server is running on port ${PORT}`);
+    });
+  });
---
+++ prisma/migrations/20250312091206_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Item" ADD COLUMN     "baseline" INTEGER NOT NULL DEFAULT 10;
